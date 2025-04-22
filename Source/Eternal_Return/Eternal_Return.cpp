a665af6851c15fa9798f6532d2d9fa41e7d09ce4
MYOUNGSEUNGHO <myoung6458@naver.com>
2025-04-22 21:23:51 +0900

    Init

---
+++ Source/Eternal_Return/Eternal_Return.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Eternal_Return.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Eternal_Return, "Eternal_Return" );
